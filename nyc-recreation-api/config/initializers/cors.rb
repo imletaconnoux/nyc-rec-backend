e95e5155ca40f95f1de3cb7ee219234943b9b80c
imletaconnoux <imletaconnoux@gmail.com>
2017-09-28 15:10:06 -0400

    fixed backend cors issues

--- nyc-recreation-api/config/initializers/cors.rb
+++ nyc-recreation-api/config/initializers/cors.rb
@@ -4,18 +4,18 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-
-module NYCRecreationAPI
-  class Application < Rails::Application
-    config.load_defaults 5.1
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      end
-    end
-    config.api_only = true
-  end
-end
+# 
+# module NYCRecreationAPI
+#   class Application < Rails::Application
+#     config.load_defaults 5.1
+#     config.middleware.insert_before 0, Rack::Cors do
+#       allow do
+#         origins '*'
+#         resource '*',
+#           headers: :any,
+#           methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#       end
+#     end
+#     config.api_only = true
+#   end
+# end
