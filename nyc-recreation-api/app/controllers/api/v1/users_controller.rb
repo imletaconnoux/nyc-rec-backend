e6b5dda21bd53378cd1ffeb439124eeba89bb704
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-10-02 15:12:54 -0400

    added method for loading user prefs

--- nyc-recreation-api/app/controllers/api/v1/users_controller.rb
+++ nyc-recreation-api/app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,8 @@ class Api::V1::UsersController < ApplicationController
   def me
     #should be able to retrieve user and their prefs
     if @user
-      render json: {user: @user, user_prefs: @user.user_prefs}, status: 201
+      prefs = load_user_prefs(@user)
+      render json: {user: @user, prefs: prefs}, status: 201
     else
       render json: {message: "Error"}
     end
@@ -27,6 +28,26 @@ class Api::V1::UsersController < ApplicationController
   private
   def user_params
     params.permit(:username, :password)
+  end
+
+  def load_user_prefs(user)
+    pref_objs = {zoos: [], bbqs: [], tennis: [], pools: [] }
+    user.user_prefs.each do |user_pref|
+       if user_pref[:zoo_id]
+         zoo = Zoo.all.find(user_pref[:zoo_id])
+         pref_objs[:zoos].push({zoo_info: zoo})
+       elsif user_pref[:tenni_id]
+         tenni = Tennis.all.find(user_pref[:tenni_id])
+         pref_objs[:tennis].push({tenni_info: tenni})
+       elsif user_pref[:bbq_id]
+         bbq = Bbq.all.find(user_pref[:bbq_id])
+         pref_objs[:bbqs].push({bbq_info: bbq})
+       elsif user_pref[:pool_id]
+         pool = Pool.all.find(user_pref[:pool_id])
+         pref_objs[:pools].push({pool_info: pool})
+       end
+     end
+     return pref_objs
   end
 
 end
--- nyc-recreation-api/app/controllers/application_controller.rb
+++ nyc-recreation-api/app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::API
 
   def auth_header
     header = request.headers['Authorization']
+
   end
 
   def decoded_token
